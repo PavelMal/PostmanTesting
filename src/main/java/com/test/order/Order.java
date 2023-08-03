fbf2aabc94ad00279cbc82305e4a84a4c519bb8b
pavelmal <mashino@mail.ru>
2023-08-03 16:12:06 +0800

    add order entity

---
+++ src/main/java/com/test/order/Order.java
@@ -0,0 +1,55 @@
+package com.test.order;
+
+import com.google.gson.Gson;
+
+import java.util.Objects;
+
+public class Order {
+
+    private String id;
+    private String name;
+    private String price;
+
+    public Order(String id, String name, String price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Order setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Order setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public Order setPrice(String price) {
+        this.price = price;
+        return this;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+}
