b8d56bb7f52f1dd83ce4d17c6367699d530cb356
pavelmal <mashino@mail.ru>
2023-08-03 15:53:47 +0800

    git init

---
+++ src/main/java/com/test/controller/OrderController.java
@@ -0,0 +1,17 @@
+package com.test.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+//    @PostMapping("/create")
+//    public ResponseEntity createOrder(@RequestBody Order order) {
+//        return
+//    }
+}
