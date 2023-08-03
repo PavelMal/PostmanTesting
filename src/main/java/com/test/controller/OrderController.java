25e751f2a718d27ed180cc4649725a77239afb04
pavelmal <mashino@mail.ru>
2023-08-03 16:38:54 +0800

    finish with createOrder in order controller class

--- src/main/java/com/test/controller/OrderController.java
+++ src/main/java/com/test/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.test.controller;
 
+import com.test.model.Order;
+import com.test.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/order")
 public class OrderController {
 
-//    @PostMapping("/create")
-//    public ResponseEntity createOrder(@RequestBody Order order) {
-//        return
-//    }
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/create")
+    public ResponseEntity createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
+    }
 }
