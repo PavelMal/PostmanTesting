61994727c8929ddefe110fdd05624f86d659aea8
pavelmal <mashino@mail.ru>
2023-08-08 15:31:46 +0800

    optimize imports

--- src/main/java/com/test/controller/OrderController.java
+++ src/main/java/com/test/controller/OrderController.java
@@ -3,7 +3,6 @@ package com.test.controller;
 import com.test.model.Order;
 import com.test.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
