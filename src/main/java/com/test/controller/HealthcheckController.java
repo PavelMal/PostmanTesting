0b82335cae053933836d2b7b93a83a314f80daad
pavelmal <mashino@mail.ru>
2023-08-10 12:18:36 +0800

    add healthcheck class

---
+++ src/main/java/com/test/controller/HealthcheckController.java
@@ -0,0 +1,9 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/check")
+public class HealthcheckController {
+}
