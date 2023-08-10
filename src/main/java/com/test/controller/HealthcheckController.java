f7280cf623ee21de3c4476e331c097a98e1129d5
pavelmal <mashino@mail.ru>
2023-08-10 12:22:58 +0800

    add healthcheck method

--- src/main/java/com/test/controller/HealthcheckController.java
+++ src/main/java/com/test/controller/HealthcheckController.java
@@ -1,9 +1,17 @@
 package com.test.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/check")
+@RequestMapping("/api")
 public class HealthcheckController {
+
+    @GetMapping("/check")
+    public ResponseEntity getHealthcheck() {
+        return ResponseEntity.ok().build();
+    }
 }
